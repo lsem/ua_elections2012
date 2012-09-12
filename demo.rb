4af4be49abb19b1316c45d82c49e0e29474f2100
Lyubomyr Semkiv <lyubomyr.semkiv@eleks.com>
2012-09-12 05:12:31 -0900

    Added creating test record on getting hi page was test purposes.

--- demo.rb
+++ demo.rb
@@ -2,14 +2,28 @@ require 'sinatra'
 require 'mongoid'
 require "sinatra/reloader" if development?
 
+
 configure do
 	Mongoid.configure do |config|
 		Mongoid.load!('mongoid.yml')
 	end
 end
 
+class TestRecord
+    include Mongoid::Document
+    
+    field :code, type: String
+    field :sector,    type: String
+    field :share_id, type: Integer
+    field :jse_code, type: Integer
+end
+
+
 get '/hi' do
 	content_type :json
-	"greetings!"
+	rec = TestRecord.new
+	rec.sector = 'test_sector'
+	rec.to_json
+#	"greetings!"
 end
 
