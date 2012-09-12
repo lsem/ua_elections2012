9928506b8db72f7e76826e7d8f490769a05ddce0
Lyubomyr Semkiv <lyubomyr.semkiv@eleks.com>
2012-09-12 05:10:09 -0900

    Added mongoid support (works fine locally)

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ ruby "1.9.3" # required by mongoid
 
 gem "bson_ext", "~> 1.7.0"
 
+gem "mongoid", "~> 3.0.0"
+
 gem 'sinatra'
--- demo.rb
+++ demo.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'mongoid'
+require "sinatra/reloader" if development?
+
+configure do
+	Mongoid.configure do |config|
+		Mongoid.load!('mongoid.yml')
+	end
+end
 
 get '/hi' do
-	"Hello world!"
+	content_type :json
+	"greetings!"
 end
 
